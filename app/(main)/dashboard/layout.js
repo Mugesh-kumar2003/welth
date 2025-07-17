9fc75903c5cb3765ca87041394622a7c986d5991
Mugesh Kumar K <mugeshkumarkcse2003@gmail.com>
2025-07-17 11:34:18 +0530

    Added few Changes

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -9,7 +9,7 @@ const DashboardLayout = () => {
 
       {/*  DashBoard Page */}
       <Suspense 
-        fallback={<BarLoader lassName="mt-4" width={"100%"} color="#9333ea" />}
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
       >
         <DashboardPage />
       </Suspense>
